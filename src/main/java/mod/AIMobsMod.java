31f2c247c2784b7edb056b8331a249806adb44d7
komob <b.komorniczak@gmail.com>
2024-01-10 08:42:19 +0100

    Configure initial settings

--- src/main/java/mod/AIMobsMod.java
+++ src/main/java/mod/AIMobsMod.java
@@ -5,6 +5,6 @@ import net.fabricmc.api.ClientModInitializer;
 public class AIMobsMod implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+//to implement
     }
 }
